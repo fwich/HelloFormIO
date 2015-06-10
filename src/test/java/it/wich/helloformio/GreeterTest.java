9f945de0bc88ad17db731a2ea41fd358ad1904f9
Florian Wich <florian.wich@gmail.com>
2015-06-10 20:22:34 +0200

    corrected source folders, wrote first unittest

---
+++ src/test/java/it/wich/helloformio/GreeterTest.java
@@ -0,0 +1,14 @@
+package it.wich.helloformio;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GreeterTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
